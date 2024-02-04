9cfab92d60f1d1a8ddcb05d8eb601ed0a66693cd
fr33m0nk <prashantsinha@protonmail.com>
2024-02-04 17:47:06 +0530

    Adds test for `with-shutdown-on-failure`

---
+++ test/fr33m0nk/structured_task_scope_test.clj
@@ -0,0 +1,39 @@
+(ns fr33m0nk.structured-task-scope-test
+  (:require [clojure.test :refer :all]
+            [fr33m0nk.structured-task-scope :as sts])
+  (:import [java.time Instant]))
+
+(deftest shutdown-on-failure-test
+  (testing "returns exception if :throw-on-failure? is set in options"
+    (is (instance? Exception (sts/with-shutdown-on-failure
+                               scope
+                               [turtle (sts/forker scope (Thread/sleep 5000) :turtle-wins)
+                                hare (sts/forker scope (throw (ex-info "boom" {})))]
+                               {}
+                               (.exception hare)))))
+
+  (testing "throws exception if :throw-on-failure? is set in options"
+    (is (thrown? Exception (sts/with-shutdown-on-failure
+                             scope
+                             [turtle (sts/forker scope (Thread/sleep 5000) :turtle-wins)
+                              hare (sts/forker scope (throw (ex-info "boom" {})))]
+                             {:throw-on-failure? true}
+                             (map #(name (.get %)) [turtle hare])))))
+
+  (testing "throws exception if :deadline-instant is set in options and it elapses"
+    (is (thrown? Exception (sts/with-shutdown-on-failure
+                             scope
+                             [turtle (sts/forker scope (Thread/sleep 5000) :turtle-wins)
+                              hare (sts/forker scope (Thread/sleep 5000) :hare-wins)]
+                             {:deadline-instant (.plusMillis (Instant/now) 2000)}
+                             (map #(name (.get %)) [turtle hare])))))
+
+  (testing "forks subtasks mentioned in forked bindings that can be used in body"
+    (is (= ["turtle-wins" "hare-wins"]
+           (sts/with-shutdown-on-failure
+             scope
+             [turtle (sts/forker scope (Thread/sleep 5000) :turtle-wins)
+              hare (sts/forker scope (Thread/sleep 5000) :hare-wins)]
+             {:throw-on-failure? true
+              :deadline-instant (.plusMillis (Instant/now) 7000)}
+             (map #(name (.get %)) [turtle hare]))))))
